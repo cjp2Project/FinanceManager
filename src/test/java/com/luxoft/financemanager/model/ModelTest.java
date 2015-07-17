8694a6399026bddb2db3dc1218ee9c2e1617d0ec
djedynak <damianjed90@gmail.com>
2015-07-17 11:05:19 +0200

    added .gitignore

--- src/test/java/com/luxoft/financemanager/model/ModelTest.java
+++ src/test/java/com/luxoft/financemanager/model/ModelTest.java
@@ -1,7 +1,4 @@
 package com.luxoft.financemanager.model;
 
-/**
- * Created by djedynak on 2015-07-17.
- */
 public class ModelTest {
 }
--- src/test/java/com/luxoft/financemanager/service/ServiceTest.java
+++ src/test/java/com/luxoft/financemanager/service/ServiceTest.java
@@ -1,7 +1,5 @@
 package com.luxoft.financemanager.service;
 
-/**
- * Created by djedynak on 2015-07-17.
- */
+
 public class ServiceTest {
 }
